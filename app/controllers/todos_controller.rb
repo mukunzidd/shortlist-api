45daec672d8aeb07e610730259c041ee5c39967b
Dodo <dmukunzi0@gmail.com>
2018-10-19 15:18:14 +0200

    feat(*): add specs for /todos/:id and implement the feature

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,22 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
   def index
     @todos = Todo.all
     json_response(@todos)
+  end
+
+  def show
+    json_response(@todo)
+  end
+
+  private
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    # Params whitelist
+    params.permit(:title, :created_by)
   end
 end
